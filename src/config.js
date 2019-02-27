107bfcaa39d7dd30c7a8404b863ec377513d4cbe
Alexandr Bondarchuk <alex.coockoo@gmail.com>
2019-02-27 14:46:28 +0200

    Remove invalid code

--- src/config.js
+++ src/config.js
@@ -11,18 +11,5 @@ module.exports = {
   port: argv.port || 3000,
   adAccountId: `act_${argv.id}`,
   token: argv.token,
-
-  // Development account
-  // token:
-  // 'EAAcHfIID1jIBAH2YcS2BwZBZACtz6ayGmDfIL4ihcJoWT4hwxZCGqLO6ic9nlmSU5YxNhtjmP6i8igQ3bV2ZAXwKIYdRViDZBMJ6Gut7EAdBmGu2sj7UhLQC0ZBZA1KBeVSKsR4WX8XRjKwbcBmZA7pZAwEQiuK4UlELY7hdxQgrTCXD6vz8ckn3ZB2ZCnMHLjzgMIZD',
-  // addAccountId: 'act_244915619722504',
-
-  // Personal account
-  //493190961147083
-  //addAccountId: `act_${}`,
-  token:
-    'EAAcHfIID1jIBACVDEYA3GPa22QZAZC5c7ZBD5TNucX8p7ulX52ehke8wz4Lo8Q3egWAcU2DwLoQAHaHL8qH8HNIZChAg000bi3nKgv46lScsaLEyG6EBvZAERZBe4Tl1lWJkbmbOX1bqECDjoRv9XmqOWU1T32089QwZCFb1a6lZBQZDZD',
-
-  // addAccountId: '352697805330464',
   mongoUrl: argv.mongo || 'mongodb://localhost:27017',
 };
